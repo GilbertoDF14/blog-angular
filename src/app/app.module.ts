f331d8e5acc771c7b6f54c18b83c43e6a8f1f6fe
GilbertoDF14 <48808299+GilbertoDF14@users.noreply.github.com>
2021-11-01 13:57:39 -0600

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
